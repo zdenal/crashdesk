36f42f9f655b529109409bd6148a397c18443b16
Zdenko Nevrala <nevralaz@gmail.com>
2012-06-12 11:59:42 +0200

    Update errors.

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -3,7 +3,7 @@ class ErrorsController < ApplicationController
   respond_to :json, :html
 
   def index
-    session[:app_id] ||= Bucket.first.name
+    session[:app_id] ||= Bucket.last.name
     @errors = Error.find(:all, params: { app_id: session[:app_id] })
     respond_with @errors
   end
@@ -15,17 +15,14 @@ class ErrorsController < ApplicationController
 
   def update
     @error = Error.find(params[:id], params: { app_id: session[:app_id] })
-    #@error = Error.new
-    #@error.load params[:error]
-    #@error.prefix_options = {app_id: session[:app_id]}
-    debugger
+    @error.load params[:error]
+    @error.prefix_options = {app_id: session[:app_id]}
     @error.save
     respond_with status: :ok
-    #respond_with Error.update params[:id], params[:app]
   end
 
   def destroy
-    #respond_with Error.destroy params[:id]
+    respond_with Error.destroy params[:id]
     respond_with status: :ok
   end
 
